a950b3d4ec836a46201bd3b13b6ebfb747d366a1
Gabriela Alvarenga <gabrielasalvarenga2@gmail.com>
2024-08-15 14:15:21 -0300

    feat: implementing routes with controllers logic

--- API_para_servicos_bancarios/src/main/composer/login_creator_composer.py
+++ API_para_servicos_bancarios/src/main/composer/login_creator_composer.py
@@ -1,12 +1,12 @@
 from src.models.settings.db_connection_handler import db_connection_handler
 from src.models.repositories.user_repository import UserRepository
-from src.controllers.login_creator import LoginCreatorController
+from src.controllers.login_creator import LoginCreator
 from src.views.login_creator_view import LoginCreatorView
 
 def login_creator_composer():
     conn = db_connection_handler.get_connection()
     model = UserRepository(conn)
-    controller = LoginCreatorController(model)
+    controller = LoginCreator(model)
     view = LoginCreatorView(controller)
     
     return view
